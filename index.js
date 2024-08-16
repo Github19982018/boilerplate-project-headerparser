199e9169d326a9327a3c0dc91fff47ee86221ee4
jobin kuriakose <jobinkuriakose@protonmail.com>
2024-08-16 15:06:52 +0530

    completed

--- index.js
+++ index.js
@@ -24,7 +24,16 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami',function(req,res){
+  const resp = {'ipaddress':req.ip, 'language':req.headers['accept-language'],
+    'software':req.headers['user-agent']
+  }
+  // console.log(req.headers)
+  res.json(resp)
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
